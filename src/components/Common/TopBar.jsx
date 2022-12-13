8a2aefe334f88e72f8bc69b9809418524e424f44
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-12-13 20:12:47 +0600

    reorganized scss

---
+++ src/components/Common/TopBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BsEnvelopeFill, BsFillTelephoneFill } from 'react-icons/bs';
+
+export const TopBar = () => {
+  return (
+    <>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left row">
+            <BsFillTelephoneFill />
+            <label>+10012 0000 0000</label>
+            <BsEnvelopeFill />
+            <label>hello@sijonal.com </label>
+          </div>
+
+          <div className="right row RText">
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span>🇺🇸</span>
+            <label>EN</label>
+            <span>🇺🇸</span>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
