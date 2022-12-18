e48040255edd09255d7a8875da97058b042f9a48
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-12-18 23:02:31 +0600

    add slider

---
+++ src/data/sliderData.jsx
@@ -0,0 +1,28 @@
+import slider1 from '../assets/SlideCard/slide-1.png';
+
+export const sliderData = [
+  {
+    id: 1,
+    title: '50% Off For Your First Shopping',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.',
+    cover: slider1,
+  },
+  {
+    id: 2,
+    title: '50% Off For Your First Shopping',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.',
+    cover: slider1,
+  },
+  {
+    id: 3,
+    title: '50% Off For Your First Shopping',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.',
+    cover: slider1,
+  },
+  {
+    id: 4,
+    title: '50% Off For Your First Shopping',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.',
+    cover: slider1,
+  },
+];
