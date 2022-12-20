ad8720d30924eceb04a0926cc8629d985ee9daf8
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-12-20 23:57:06 +0600

    complete slider

--- src/data/sliderData.jsx
+++ src/data/sliderData.jsx
@@ -1,4 +1,7 @@
-import slider1 from '../assets/SlideCard/slide-1.png';
+import slider1 from '../assets/Slider/slide-1.png';
+import slider2 from '../assets/Slider/slide-2.png';
+import slider3 from '../assets/Slider/slide-3.png';
+import slider4 from '../assets/Slider/slide-4.png';
 
 export const sliderData = [
   {
@@ -11,18 +14,18 @@ export const sliderData = [
     id: 2,
     title: '50% Off For Your First Shopping',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.',
-    cover: slider1,
+    cover: slider2,
   },
   {
     id: 3,
     title: '50% Off For Your First Shopping',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.',
-    cover: slider1,
+    cover: slider3,
   },
   {
     id: 4,
     title: '50% Off For Your First Shopping',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.',
-    cover: slider1,
+    cover: slider4,
   },
 ];
