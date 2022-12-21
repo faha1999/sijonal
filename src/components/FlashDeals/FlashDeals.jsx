5f150a7cc4a6b0391cec9e491a178bcc3cfebdf2
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-12-21 23:43:12 +0600

    add FlashDeals

---
+++ src/components/FlashDeals/FlashDeals.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const FlashDeals = () => {
+  return (
+    <>
+      <section className="flash">
+        <div className="container">
+          <div className="heading f_flex">
+            <i className="fa fa-bolt"></i>
+            <h1>Flash Delas</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { FlashDeals } from '../components/FlashDeals/FlashDeals';
 import { Home } from '../components/home/Home';
 
 export const MainPage = () => {
   return (
     <>
       <Home />
+      <FlashDeals />
     </>
   );
 };
