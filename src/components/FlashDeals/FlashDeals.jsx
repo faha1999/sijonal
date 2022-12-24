58cf0c2e314506047f3693e6a4e2cb3645e082b9
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-12-24 21:35:49 +0600

    fixed issue

--- src/components/FlashDeals/FlashDeals.jsx
+++ src/components/FlashDeals/FlashDeals.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { TiFlash } from 'react-icons/ti';
 import { FlashCard } from './FlashCard';
 
-export const FlashDeals = ({ CartItem }) => {
+export const FlashDeals = ({ flashData, addToCart }) => {
   return (
     <>
       <section className="flash">
         <div className="container">
           <div className="heading f_flex">
             <TiFlash style={{ color: '#fa6a27', margin: '.625rem' }} />
-            <h1>Flash Delas</h1>
+            <h1>Flash Deals</h1>
           </div>
-          <FlashCard CartItem={CartItem} />
+          <FlashCard flashData={flashData} addToCart={addToCart} />
         </div>
       </section>
     </>
