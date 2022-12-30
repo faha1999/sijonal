a78a38d261fbfb821bf93ea07914ceec0df95b1c
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-12-30 22:46:00 +0600

    added TopCategories

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FlashDeals } from '../components/FlashDeals/FlashDeals';
 import { Home } from '../components/home/Home';
+import { TopCategories } from '../components/topCategories/TopCategories';
 
 export const MainPage = ({ flashData, addToCart, CartItem }) => {
   return (
     <>
       <Home CartItem={CartItem} />
       <FlashDeals flashData={flashData} addToCart={addToCart} />
+      <TopCategories />
     </>
   );
 };
