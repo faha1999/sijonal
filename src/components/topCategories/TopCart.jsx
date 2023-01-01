936d053deb477866e781fd017d7ed912bbafe4a8
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-01-01 22:33:00 +0600

    complete Top Categories

--- src/components/topCategories/TopCart.jsx
+++ src/components/topCategories/TopCart.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper';
+import { topCartData } from '../../data/topCartData';
 
 export const TopCart = () => {
-  return <div>TopCart</div>;
+  return (
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={10}
+        className="mySwiper"
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination]}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+      >
+        {topCartData.map((data, index) => (
+          <SwiperSlide className="box product" key={index}>
+            <div className="nametop d_flex">
+              <span className="tleft">{data.para}</span>
+              <span className="tright">{data.desc}</span>
+            </div>
+            <div className="img">
+              <img src={data.cover} alt={data.desc} />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 };
