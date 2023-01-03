26af6313d72527f0ca5c6d027119dfec9ef1d397
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-01-03 21:58:59 +0600

    complete wrapper

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,6 +6,7 @@ import { Home } from '../components/home/Home';
 import { NewArrivals } from '../components/newArrivals/NewArrivals';
 import { Shop } from '../components/shop/Shop';
 import { TopCategories } from '../components/topCategories/TopCategories';
+import { Wrapper } from '../components/wrapper/Wrapper';
 
 export const MainPage = ({ flashData, addToCart, CartItem, shopItems }) => {
   return (
@@ -17,6 +18,7 @@ export const MainPage = ({ flashData, addToCart, CartItem, shopItems }) => {
       <Discount />
       <Shop shopItems={shopItems} addToCart={addToCart} />
       <Announcement />
+      <Wrapper />
     </>
   );
 };
